684dc48d19e1cecb6394e85a54070477f5db565f
Олександра Лавріненко <oleksandra_lavrinenko@outlook.com>
2026-05-12 09:59:49 +0300

    Created a Horse class for race participants

---
+++ Laba5.1/Horse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace Laba5._1
+{
+    public class Horse
+    {
+        // ім'я коня
+        public string Name { get; private set; }
+
+        // колір коня
+        public Brush Color { get; private set; }
+
+        // базова швидкість
+        public double Speed { get; private set; }
+
+        // позиція на трасі
+        public double X { get; set; }
+
+        // час фінішу
+        public TimeSpan FinishTime { get; set; }
+
+        public Horse(string name, Brush color)
+        {
+            Name = name;
+            Color = color;
+        }
+    }
+}
